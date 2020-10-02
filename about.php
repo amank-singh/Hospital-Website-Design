32be82457d66da96c7d43d8d66603143afe967ca
AAA Tech Services <70901376+aaatechservices@users.noreply.github.com>
2020-10-02 23:52:50 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,169 @@
+<?php require("./head.php")?>
+<section class="about-entry">
+        <div class="container">
+            <div class="row">
+              <div class="col-sm-7 offset-sm-5 text-right">
+            
+            <h6 class="entry-main-sub">APPLE HOSPITAL SIKAR</h6>
+            <h1 class="entry-main-about">ABOUT US</h1>
+            
+              </div>
+            </div>
+
+            
+
+        </div>
+      </section>
+
+      <section class="about">
+          <div class="container">
+          <h2>WELCOME TO APPLE HOSPITAL SIKAR & TEST TUBE BABY CENTER</h2>
+          <h4>BEST IN CLASS SERVICE BEST IN CLASS DOCTORS</h4>
+            <div class="row mrg50 align-items-center">
+            <div class="col-sm-6 text-justify">
+                
+              <p>Apple hospital has 5 glorious years of excellence in child care, women care and fertility. Setting the benchmark in neonatal and pediatrics intensive care, Apple Hospital has played a major role in saving thousands of critically ill children and has emerged as one of the best children’s hospitals in the city. We aim to provide high standards of care for the mother, women, fetus, new-born and children so that none of them is deprived of a tertiary care facility. The measure of our success is in the number of smiling faces. Filling your life with the pinkest of health is our dream. As a Women and Children’s Hospital, Apple Hospital strives to send you home in perfect health as a colourful life welcomes you back.</p>
+              <hr>
+              <h6>To Avail Best In Class Service</h6>
+              <a class="btn btn-3 btn-lg mrg15" href="">Contact Us</a>
+            </div>
+            <div class="col-sm-6 text-right mrgp50">
+              <img src="img/555.jpg" >
+            </div>
+            </div>
+            <br><br><br>
+            <hr>
+            <br><br><br>
+            <center>
+            
+            <h2>MEET OUR DOCTORS</h2>
+            </center>
+            <br>
+            <div class="row mrg50 doc text-center justify-content-around">
+                <div class="col-sm-5">
+                    <a data-toggle="modal" data-target="#doc1"><div class="card">
+                      <div class="card-body">
+                        <img src="./img/44 (1).jpg">
+                        <br><br>
+                        <h4>Dr. Kishore Kumar Jangir</h4>
+                        <p>M.S. (Obstretics & Gynecology)</p>
+                      </div>
+                    </div></a>
+                </div>
+                <div class="col-sm-5">
+                <a data-toggle="modal" data-target="#doc2"><div class="card">
+                      <div class="card-body">
+                        <img src="./img/44 (2).jpg">
+                        <br><br>
+                        <h4>Dr. Kapil Sharma</h4>
+                        <p>M.D. (Pediatrics)</p>
+                      </div>
+                    </div></a>
+                </div>
+            </div>
+          </div>
+
+          <br><br><br>
+            <hr>
+            <br><br><br>
+            <center>
+            
+            <h2>ACHIEVEMENTS</h2>
+            </center>
+<bR><br><br>
+
+<!-- Flickity HTML init -->
+<div class="carousel"
+  data-flickity='{ "freeScroll": true, "wrapAround": true }'>
+  <div class="carousel-cell">
+  <center><img src="img/10 (1).png" width="100%"></center>
+  </div>
+  <div class="carousel-cell"><center><img src="img/10 (2).png" width="100%"></center></div>
+  <div class="carousel-cell"><center><img src="img/10 (3).png" width="100%"></center></div>
+
+</div>
+
+
+</section>
+
+<div class="modal fade" id="doc1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content ">
+      
+      <div class="modal-body">
+      <center><img src="./img/44 (1).jpg" width="90%">
+                        <br><br>
+                        <h4>Dr. Kishore Kumar Jangir</h4>
+                        <p>M.S. (Obstretics & Gynecology)</p></center>
+      <br>
+      <p><b>QUALIFICATION</B></p>
+
+      <p>MBBS & MS (Obstetrics & Gynaecology) - RNT Medical College, Udaipur
+</p>
+<br>
+<p><b>SPECIALIZES IN</B></p>
+
+<p>1.      Obstetrics & Gynaecology<br>
+2.      Infertility<br>
+3.      Reproductive Medicine<br>
+</p>
+      <br>
+        <center><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></center>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="doc2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content ">
+      
+      <div class="modal-body">
+      <center><img src="./img/44 (2).jpg" width="90%">
+                        <br><br>
+                        <h4>Dr. Kapil Sharma</h4>
+                        <p>M.D. (Pediatrics)</p></center>
+      <br>
+      <p><b>QUALIFICATION</B></p>
+
+      <p>MBBS - B.J. Medical College, Ahmedabad<br>
+M.D.(Paediatric) - N.H.L. Medical College, Ahmedabad<br>
+Member - IAP, MMF, IMA
+
+</p>
+<br>
+<p><b>SPECIALIZES IN</B></p>
+
+<p>1.      Acute & chronic disease care<br>
+2.      Immunization and vaccines<br>
+3.      Well Child Care<br>
+4.      New born baby health check up<br>
+5.      Routine Paediatric check-up & Follow up<br>
+</p>
+      <br>
+        <center><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></center>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<?php require('./footer.php')?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://kit.fontawesome.com/1b1497c545.js" crossorigin="anonymous"></script>
+   
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+     <script src="https://npmcdn.com/flickity@2/dist/flickity.pkgd.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.4/jquery.touchSwipe.min.js"></script>
+    <script src="js/slider.js"></script>
+    
+    
+  </body>
+</html>
---
+++ contact.php
@@ -0,0 +1,134 @@
+<?php
+  ob_start();
+  require("admin/functions/db.php");
+
+    $err ="";
+
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+      if(empty(trim($_POST["message"]))){
+
+          $err = 'Please enter a message';
+
+
+      } else{
+
+          $message = trim($_POST["message"]);
+
+      }
+
+      if(empty(trim($_POST["phone"]))){
+
+          $err = 'Please enter an phone number';
+
+
+      } else{
+
+          $phone = trim($_POST["phone"]);
+
+      }
+
+      if(empty(trim($_POST['name']))){
+
+          $err = 'Please enter your name';
+
+
+      } else{
+
+          $name = trim($_POST['name']);
+
+      }
+
+      if(!$err)
+      {
+
+          $email = trim($_POST['email']);
+
+          $sql = "INSERT INTO contactus (name,phone, email, message) VALUES ('".$name."','".$phone."','".$email."','".$message."')";
+
+        if ($conn->query($sql) === TRUE) {
+            $err ="Message Sent";
+            } else {
+            $err= "Message Not Sent";
+            }
+
+
+      }
+
+}
+
+
+
+ ?>
+<?php require("./head.php")?>
+
+
+      <section class="about service">
+          <div class="container">
+            <centeR>
+          <h2>CONTACT US</h2>
+          <h4>Get in touch with us!</h4>
+            </center>
+            
+
+            <div class="row">
+          <div class="col-md-8 offset-md-2">
+            <form class="pdg50" action="contact.php" method="post">
+              <div class="row">
+                <div class="form-group col-sm-6">
+                  <label for="exampleInputEmail1">Name</label>
+                  <input type="name" class="form-control" id="exampleInputEmail1" name="name" aria-describedby="emailHelp">
+
+                </div>
+                <div class="form-group col-sm-6">
+                  <label for="exampleInputEmail1">Phone</label>
+                  <input type="phone" class="form-control" id="exampleInputEmail1" name="phone" aria-describedby="emailHelp">
+
+                </div>
+              </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp">
+
+            </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Your Message</label>
+                <textarea type="text" name="message" class="form-control" id="exampleInputPassword1"></textarea>
+              </div>
+              <center>
+              <?php
+        
+              if($err)
+              echo "<br>".$err."</br>";
+
+               ?>
+               
+              <button type="submit" class="btn btn-3 mrg25">Submit</button>
+              </center>
+            </form>
+          </div>
+        </div>
+
+ 
+
+            
+            
+          </div>
+</section>
+
+
+<?php require('./footer.php')?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js"></script>
+    <script src="https://kit.fontawesome.com/1b1497c545.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.pkgd.min.js"></script>
+    <script src="js/gallery.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
